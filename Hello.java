11dd392ed43070893929e9b701fd5986942343ad
liusx <8629303@qq.com>
2018-09-27 11:08:56 +0800

    Commit Work Dir File

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("认真听课的孩子");
+        System.out.println("不认真听课的孩子");
+        System.out.println("世界，你好！");
     }
 }
